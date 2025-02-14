cbad688ce429d6deb4ebbcbf544b4503cf8c870f
Shehta2000 <mohamedshehta819@gmail.com>
2025-02-14 23:16:25 +0200

    finish Calculator Task

--- lib/calculate_screen.dart
+++ lib/calculate_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:task2_creativa/widgets/input_section.dart';
 import 'widgets/build_section.dart';
 import 'widgets/custom_operation_button.dart';
-
 class CalculateScreen extends StatefulWidget {
   const CalculateScreen({super.key});
 
@@ -17,25 +16,20 @@ class _CalculateScreenState extends State<CalculateScreen> {
   double result = 0;
 
   void calculateResult(String operation) {
-    final double num1 = double.tryParse(num1Controller.text) ?? 0;
-    final double num2 = double.tryParse(num2Controller.text) ?? 0;
+    double num1 = double.tryParse(num1Controller.text) ?? 0;
+    double num2 = double.tryParse(num2Controller.text) ?? 0;
 
-    setState(() {
-      switch (operation) {
-        case 'add':
-          result = num1 + num2;
-          break;
-        case 'subtract':
-          result = num1 - num2;
-          break;
-        case 'multiply':
-          result = num1 * num2;
-          break;
-        case 'divide':
-          result = num2 != 0 ? num1 / num2 : 0;
-          break;
-      }
-    });
+    if (operation == 'add') {
+      result = num1 + num2;
+    } else if (operation == 'subtract') {
+      result = num1 - num2;
+    } else if (operation == 'multiply') {
+      result = num1 * num2;
+    } else if (operation == 'divide') {
+      result = (num2 != 0) ? num1 / num2 : 0;
+    }
+
+    setState(() {});
   }
 
   @override
@@ -43,18 +37,16 @@ class _CalculateScreenState extends State<CalculateScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-              child: InputSection(
-                num1Controller: num1Controller,
-                num2Controller: num2Controller,
-              ),
+        body: Column(
+          children: [
+            InputSection(
+              num1Controller: num1Controller,
+              num2Controller: num2Controller,
             ),
-            SliverToBoxAdapter(child: SizedBox(height: 40.h)),
-            SliverToBoxAdapter(child: ResultSection(result: result)),
-            SliverToBoxAdapter(child: SizedBox(height: 50.h)),
-            SliverToBoxAdapter(child: _buildOperationButtons()),
+            SizedBox(height: 40.h),
+            ResultSection(result: result),
+            SizedBox(height: 50.h),
+            _buildOperationButtons(),
           ],
         ),
       ),
@@ -62,26 +54,28 @@ class _CalculateScreenState extends State<CalculateScreen> {
   }
 
   Widget _buildOperationButtons() {
-    return Container(
-      height: 350.h,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: const Color(0xff007C6A),
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(40.r),
-          topRight: const Radius.circular(40),
+    return Expanded(
+      child: Container(
+        height: 350.h,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: const Color(0xff007C6A),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40.r),
+            topRight: const Radius.circular(40),
+          ),
         ),
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 100.h, horizontal: 25.w),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            CustomOperationButton(icon: Icons.add, onPressed: () => calculateResult('add')),
-            CustomOperationButton(icon: Icons.remove, onPressed: () => calculateResult('subtract')),
-            CustomOperationButton(icon: Icons.close, onPressed: () => calculateResult('multiply')),
-            CustomOperationButton(icon: '/', onPressed: () => calculateResult('divide')),
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 100.h, horizontal: 25.w),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CustomOperationButton(icon: Icons.add, onPressed: () => calculateResult('add')),
+              CustomOperationButton(icon: Icons.remove, onPressed: () => calculateResult('subtract')),
+              CustomOperationButton(icon: Icons.close, onPressed: () => calculateResult('multiply')),
+              CustomOperationButton(icon: '/', onPressed: () => calculateResult('divide')),
+            ],
+          ),
         ),
       ),
     );
